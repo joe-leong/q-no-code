aa982bcca422c3090190d3bfa906e34ae732dc14
joeaaa <zdnf_lgq@163.com>
2023-07-23 15:50:22 +0800

    feat: 🎸 stylelint

--- src/App.vue
+++ src/App.vue
@@ -32,10 +32,10 @@ header {
 }
 
 nav {
+  margin-top: 2rem;
   width: 100%;
   font-size: 12px;
   text-align: center;
-  margin-top: 2rem;
 }
 
 nav a.router-link-exact-active {
@@ -48,19 +48,21 @@ nav a.router-link-exact-active:hover {
 
 nav a {
   display: inline-block;
-  padding: 0 1rem;
   border-left: 1px solid var(--color-border);
+  padding: 0 1rem;
 }
 
 nav a:first-of-type {
   border: 0;
 }
 
-@media (min-width: 1024px) {
+@media (width >= 1024px) {
   header {
     display: flex;
-    place-items: center;
+    border: 1px solid red;
     padding-right: calc(var(--section-gap) / 2);
+    color: red;
+    place-items: center;
   }
 
   .logo {
@@ -74,12 +76,11 @@ nav a:first-of-type {
   }
 
   nav {
-    text-align: left;
-    margin-left: -1rem;
-    font-size: 1rem;
-
-    padding: 1rem 0;
     margin-top: 1rem;
+    margin-left: -1rem;
+    padding: 1rem 0;
+    font-size: 1rem;
+    text-align: left;
   }
 }
 </style>
